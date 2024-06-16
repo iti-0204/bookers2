484d1a34b15748e21f25e533275ffc85ca5d87c2
iti-0204 <84443576+iti-0204@users.noreply.github.com@users.noreply.github.com>
2024-06-16 08:39:31 +0000

    [Add] 一部テンプレート化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
   def new
+    
   end
 
   def index
+    @user = User.find(current_user.id)
+    @books = @user.books
+    @book = Book.new
   end
 
   def show
