1746cc8550fc22dac930d9278eac2aa4fc962ba7
iti-0204 <84443576+iti-0204@users.noreply.github.com@users.noreply.github.com>
2024-06-18 07:39:54 +0000

    [Add] destroy機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:new, :index, :show, :create, :edit, :update ]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy ]
   resources :users, only: [:show, :edit, :index, :create]
 
   get 'homes/about' => "homes#about", as: "about"
